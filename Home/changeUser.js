8abaaee7eb1bcdd8937286762adfcd9115d75c68
Ghulam habib <ghulamhabib.x@gmail.com>
2025-04-20 23:11:32 +0500

    new thing

---
+++ Home/changeUser.js
@@ -0,0 +1,8 @@
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+    let myname;
+    myname=prompt("Enter Name ");
+    myHeading.innerText=myname;
+
+}
